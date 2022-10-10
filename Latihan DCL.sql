b6e2389555a6630a741af7d7958c484b1a598280
Ciel24 <115457385+Ciel24@users.noreply.github.com>
2022-10-10 17:05:09 +0800

    Update Latihan DCL.sql

--- Latihan DCL.sql
+++ Latihan DCL.sql
@@ -1,9 +1,9 @@
 CREATE USER 'DosenSisfor'@'localhost' IDENTIFIED BY '12345';
 
-GRANT ALL PRIVILEGES 
-ON praktisi
-TO 'DosenSisfor'@'localhost';
+ GRANT ALL PRIVILEGES 
+ ON praktisi
+ TO 'DosenSisfor'@'localhost';
 
-GRANT SELECT 
-ON praktisi
-TO '*'@'localhost';
+ GRANT SELECT 
+ ON praktisi
+ TO '*'@'localhost';
